fded818e21769288bff941ef43312718fcf5f58d
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-04-30 04:11:25 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,14 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+  
+  def activate
+    @student = Student.find(params[:id])
+    @student.activate
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
